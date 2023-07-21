f824289e86785440ca5d0df88782577444b4c06f
Artur Mylnikov <kuraidatenshidesu@gmail.com>
2023-07-21 09:24:47 +0300

    localStorage done

--- practise_task_two/main.ts
+++ practise_task_two/main.ts
@@ -519,18 +519,57 @@ function renameTaskInStorage(task: ITask, name: string): void {
         let task_done_index = tasks_done_names.indexOf(task.title);
         let task_current_index = tasks_current_names.indexOf(task.title);
 
+        console.log(task_available_index);
+        console.log(task_done_index);
+        console.log(task_current_index);
+        
+
         if (task_available_index !== -1) {
-            tasks_available_names[task_available_index] = name;
+            tasks_available_parsed[task_available_index].title = name;
+            tasks_available_parsed[task_available_index].html =  `
+                <div class="task task-${tasks_available_parsed[task_available_index].type}">
+                    <h2 class="task_name">${tasks_available_parsed[task_available_index].title}</h2>
+                    <p class="task_date">${tasks_available_parsed[task_available_index].date}</p>
+    
+                    <a class="task_button task_button__edit"></a>
+                    <a class="task_button task_button__done"></a>
+                    <a class="task_button task_button__remove"></a>
+                </div>
+            `,
+
+            localStorage.setItem(key_available, JSON.stringify(tasks_available_parsed));
             return;
         }
 
         if (task_done_index !== -1) {
-            tasks_done_names[task_done_index] = name;
+            tasks_done_parsed[task_done_index].title = name;
+            tasks_done_parsed[task_done_index].html =  `
+                <div class="task task-${tasks_done_parsed[task_done_index].type}">
+                    <h2 class="task_name">${tasks_done_parsed[task_done_index].title}</h2>
+                    <p class="task_date">${tasks_done_parsed[task_done_index].date}</p>
+    
+                    <a class="task_button task_button__edit"></a>
+                    <a class="task_button task_button__done"></a>
+                    <a class="task_button task_button__remove"></a>
+                </div>
+            `,
+            localStorage.setItem(key_done, JSON.stringify(tasks_done_parsed))
             return;
         }
 
         if (task_current_index !== -1) {
-            tasks_current_names[task_current_index] = name;
+            tasks_current_parsed[task_current_index].title = name;
+            tasks_current_parsed[task_current_index].html =  `
+                <div class="task task-${tasks_current_parsed[task_current_index].type}">
+                    <h2 class="task_name">${tasks_current_parsed[task_current_index].title}</h2>
+                    <p class="task_date">${tasks_current_parsed[task_current_index].date}</p>
+    
+                    <a class="task_button task_button__edit"></a>
+                    <a class="task_button task_button__done"></a>
+                    <a class="task_button task_button__remove"></a>
+                </div>
+            `,
+            localStorage.setItem(key_available, JSON.stringify(tasks_current_parsed))
             return;
         }
     }
